5b3b20af19a4a7b8af3f1a3ddfe7976893851f95
JoujonikiAsa2 <nibeditaroy582@gmail.com>
2025-01-09 12:37:50 +0600

    update admin route authorization

--- src/app/moduls/admin/admin.route.ts
+++ src/app/moduls/admin/admin.route.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import { adminControllers } from './admin.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { updateAdminValidationSchema } from './admin.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 const router = Router();
 
-router.get('/', adminControllers.getAllAdmin);
-router.get('/:id', adminControllers.getSingleAdmin);
-router.patch('/:id', validateRequest(updateAdminValidationSchema) , adminControllers.updateSingleAdmin);
-router.delete('/:id', adminControllers.deleteSingleAdmin);
+router.get('/', auth(USER_ROLE.superAdmin), adminControllers.getAllAdmin);
+router.get('/:id', auth(USER_ROLE.superAdmin), adminControllers.getSingleAdmin);
+router.patch('/:id', auth(USER_ROLE.superAdmin), validateRequest(updateAdminValidationSchema) , adminControllers.updateSingleAdmin);
+router.delete('/:id', auth(USER_ROLE.superAdmin), adminControllers.deleteSingleAdmin);
 
 export const adminRoutes = router;
